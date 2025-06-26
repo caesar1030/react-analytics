cdacdaf7a7bd75f0904d77463cee9c8dd6684207
박완섭 <wanseob.dev@gmail.com>
2025-06-27 01:25:47 +0900

    test. GAConfig unit test

---
+++ src/component/__tests__/GAConfig.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it } from 'vitest'
+import { GAProvider } from '@/provider/GAProvider'
+import { useGAConfig } from '@/hook'
+import { GAConfig } from '@/index'
+
+vi.mock('@/hook/useGAConfig', () => ({
+  useGAConfig: vi.fn(),
+}))
+
+describe('<GAConfig />', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('children을 렌더링해야 한다.', () => {
+    render(
+      <GAProvider measurementId="G-TEST123">
+        <GAConfig config={{ page_title: 'title' }}>
+          <div>child</div>
+        </GAConfig>
+      </GAProvider>,
+    )
+
+    expect(screen.getByText('child')).toBeInTheDocument()
+  })
+
+  it('렌더링이 되면 useGAConfig 훅을 호출해야 한다.', () => {
+    render(
+      <GAProvider measurementId="G-TEST123">
+        <GAConfig config={{ page_title: 'title' }}>
+          <div>child</div>
+        </GAConfig>
+      </GAProvider>,
+    )
+
+    expect(useGAConfig).toHaveBeenCalled()
+  })
+})
